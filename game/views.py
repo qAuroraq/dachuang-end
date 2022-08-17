c97439fa198544aed93f8ea403da351a7bd0b496
loveqq <1175221991@qq.com>
2022-08-17 16:50:14 +0800

    first index

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line = '<h1 style="text-align: center">开始</h1>'
+    line2 = '<img src="https://img2.baidu.com/it/u=2532177699,3005981499&fm=253&fmt=auto&app=120&f=PNG?w=640&h=433">'
+    return HttpResponse(line + line2)
