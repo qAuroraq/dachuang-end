d40de6c5463cd187d5c0f5de18be4bc5c3023b35
loveqq <1175221991@qq.com>
2022-10-03 15:08:38 +0800

    write menu page

--- game/urls/index.py
+++ game/urls/index.py
@@ -3,7 +3,7 @@ from game.views.index import index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("menu/", inclue("game.urls,menu.index")),
+    path("menu/", include("game.urls.menu.index")),
     path("playground/", include("game.urls.playground.index")),
     path("settings/", include("game.urls.settings.index")),
 ]
